a848c2ca1e720567d0f354a40a163ab9fd2cd9ea
Khanhbroo <khanhmathias532211@gmail.com>
2025-10-21 16:06:14 +0700

    feat: adding GenderCollectionSection component for Homepage

---
+++ frontend/src/components/Products/GenderCollectionSection.tsx
@@ -0,0 +1,53 @@
+import { Link } from "react-router";
+import menCollectionImage from "/men-collection.webp";
+import womenCollectionImage from "/women-collection.webp";
+
+const GenderCollectionSection = () => {
+  return (
+    <section className="py-16 px-4 lg:px-0 ">
+      <div className="container mx-auto flex flex-col md:flex-row gap-8">
+        {/* Women Collection */}
+        <div className="relative flex-1 rounded-2xl overflow-hidden">
+          <img
+            src={womenCollectionImage}
+            alt="Women's collection"
+            className="w-full h-[400px] md:h-[500px] xl:h-[700px] object-cover hover:brightness-75 duration-300 transition-all"
+          />
+          <div className="absolute bottom-8 left-8 bg-white/90 p-4 mb-3 rounded-lg">
+            <h2 className="text-2xl font-bold text-gray-900">
+              Women&apos;s Collection
+            </h2>
+            <Link
+              to="/collections/all?gender=Women"
+              className="text-gray-900 hover:text-gray-700 transition-colors underline"
+            >
+              Shop Now
+            </Link>
+          </div>
+        </div>
+
+        {/* Men Collection */}
+        <div className="relative flex-1 rounded-2xl overflow-hidden">
+          <img
+            src={menCollectionImage}
+            alt="Men's collection"
+            className="w-full h-[400px] md:h-[500px] xl:h-[700px] object-cover hover:brightness-75 duration-300 transition-all"
+          />
+          <div className="absolute bottom-8 left-8 bg-white/90 p-4 mb-3 rounded-lg">
+            <h2 className="text-2xl font-bold text-gray-900 hover:text-gray-700 transition-colors">
+              Men&apos;s Collection
+            </h2>
+            <Link
+              to="/collections/all?gender=Men"
+              className="text-gray-900 underline"
+            >
+              Shop Now
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default GenderCollectionSection;
