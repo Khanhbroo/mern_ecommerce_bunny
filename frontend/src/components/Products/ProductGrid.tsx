c2e37d0689f73b7874672b4ace125f3d0314c369
Khanhbroo <khanhmathias532211@gmail.com>
2025-10-23 16:09:10 +0700

    feat: adding FeaturedCollection Component to HomePage

--- frontend/src/components/Products/ProductGrid.tsx
+++ frontend/src/components/Products/ProductGrid.tsx
@@ -2,17 +2,16 @@ import { Link } from "react-router";
 
 const ProductGrid = ({ products }) => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 space-x-6">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-12">
       {products.map((product, index) => (
-        <div className="flex flex-col py-4">
+        <div className="flex flex-col py-4" key={index}>
           <Link
-            key={index}
             to={`/product/${product._id}`}
             className="block mb-4"
             target="_blank"
           >
             <div className="bg-white rounded-lg">
-              <div className="w-full h-96">
+              <div className="w-full h-80">
                 <img
                   src={product.images[0].url}
                   alt={product.images[0].url || product.name}
