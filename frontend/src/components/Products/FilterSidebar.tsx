9b24f8fed79a4bd2400a66586365ebd2477e7355
Khanhbroo <khanhmathias532211@gmail.com>
2025-10-25 12:01:40 +0700

    feat: adding filter logic and a little UI

--- frontend/src/components/Products/FilterSidebar.tsx
+++ frontend/src/components/Products/FilterSidebar.tsx
@@ -1,5 +1,92 @@
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router";
+import type { filtersSidebar } from "../../type/filtersSidebar";
+
 const FilterSidebar = () => {
-  return <div>FilterSidebar</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [filters, setFitlers] = useState<filtersSidebar>({
+    category: "",
+    gender: "",
+    color: "",
+    size: [],
+    material: [],
+    brand: [],
+    minPrice: 0,
+    maxPrice: 100,
+  });
+  const [priceRange, setPriceRange] = useState<Array<number>>([0, 100]);
+
+  const categories = ["Top Wear", "Bottom Wear"];
+  const colors = [
+    "Red",
+    "Blue",
+    "Black",
+    "Green",
+    "Yellow",
+    "Gray",
+    "White",
+    "Pink",
+    "Beige",
+    "Navy",
+  ];
+  const sizes = ["XS", "S", "M", "L", "XL", "XXL"];
+  const materials = [
+    "Cotton",
+    "Wool",
+    "Denim",
+    "Polyester",
+    "Silk",
+    "Linen",
+    "Viscose",
+    "Fleece",
+  ];
+  const brands = [
+    "Urban Rabbit",
+    "Hop Couture",
+    "BunnyWear",
+    "Fluffy Threads",
+    "Midnight Bunny",
+    "Carrot & Co",
+  ];
+  const genders = ["Men", "Women"];
+
+  useEffect(() => {
+    const params = Object.fromEntries([...searchParams]);
+
+    setFitlers({
+      category: params.category || "",
+      gender: params.gender || "",
+      color: params.color || "",
+      size: params.size ? params.size.split(",") : [],
+      material: params.material ? params.material.split(",") : [],
+      brand: params.brand ? params.band.split(",") : [],
+      minPrice: parseFloat(params.minPrice) || 0,
+      maxPrice: parseFloat(params.maxPrice) || 0,
+    });
+    setPriceRange([filters.minPrice, filters.maxPrice]);
+  }, [searchParams]);
+
+  return (
+    <div className="p-4">
+      <h3 className="text-xl font-medium text-gray-800 mb-4">Filter</h3>
+
+      {/* Category Filter */}
+      <div className="mb-6">
+        <label className="block text-gray-600 font-medium mb-2">Category</label>
+        {categories.map((category) => (
+          <div key={category} className="flex items-center mb-1">
+            <input
+              type="radio"
+              name="category"
+              className="mr-2 h-4 w-4 text-blue-500 focus:ring-blue-400 border-gray-300"
+            />
+
+            <span className="text-gray-700">{category}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default FilterSidebar;
---
+++ frontend/src/type/filtersSidebar.ts
@@ -0,0 +1,10 @@
+export type filtersSidebar = {
+  category: string;
+  gender: string;
+  color: string;
+  size: string[];
+  material: string[];
+  brand: string[];
+  minPrice: number;
+  maxPrice: number;
+};
