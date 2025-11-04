8362bc0626b5492bfaf36ccd32f3d204dbe85288
Khanhbroo <khanhmathias532211@gmail.com>
2025-11-05 02:11:34 +0700

    feat: update router put for products

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -38,7 +38,7 @@ router.post("/", verifyUser, checkIsAdmin, async (req, res) => {
     });
 
     if (product) {
-      return res.status(400).send("Duplicate item!");
+      return res.status(409).send("Duplicate item!");
     }
 
     product = new Product({
@@ -66,6 +66,70 @@ router.post("/", verifyUser, checkIsAdmin, async (req, res) => {
 
     const createdProduct = await product.save();
     res.status(201).json(createdProduct);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// @route PUT /api/product/:id
+// @desc Update an existing product ID
+// @access Private/Admin
+router.put("/:id", verifyUser, checkIsAdmin, async (req, res) => {
+  try {
+    const {
+      name,
+      description,
+      price,
+      discountPrice,
+      countInStock,
+      category,
+      brand,
+      sizes,
+      colors,
+      collections,
+      material,
+      gender,
+      images,
+      isFeatured,
+      isPublished,
+      tags,
+      dimensions,
+      weight,
+      sku,
+    } = req.body;
+
+    // Find product by ID
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+      // Update the product fields
+      product.name = name || product.name;
+      product.description = description || product.description;
+      product.price = price || product.price;
+      product.discountPrice = discountPrice || product.discountPrice;
+      product.countInStock = countInStock || product.countInStock;
+      product.category = category || product.category;
+      product.brand = brand || product.brand;
+      product.sizes = sizes || product.sizes;
+      product.colors = colors || product.colors;
+      product.collections = collections || product.collections;
+      product.material = material || product.material;
+      product.gender = gender || product.gender;
+      product.images = images || product.images;
+      product.isFeatured = isFeatured ?? product.isFeatured;
+      product.isPublished = isPublished ?? product.isPublished;
+      product.tags = tags || product.tags;
+      product.dimensions = dimensions || product.dimensions;
+      product.weight = weight || product.weight;
+      product.sku = sku || product.sku;
+
+      //   Save the updated product
+      const updatedProduct = await product.save();
+      res.json(updatedProduct);
+    } else {
+      res.status(404).json({ message: "Product not found" });
+    }
   } catch (error) {
     console.log(error);
     res.status(500).send("Internal Server Error");
