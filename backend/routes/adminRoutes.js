f13f0d316b2a54db4ca4f5da8704f8deade11154
Khanhbroo <khanhmathias532211@gmail.com>
2025-11-17 23:57:49 +0700

    feat: adding a router to add a new user for admin

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -18,4 +18,24 @@ router.get("/users", verifyUser, checkIsAdmin, async (req, res) => {
   }
 });
 
+// @route POST /api/admin/users
+// @desc Add a new user
+// @access Private/Admin
+router.post("/users", verifyUser, checkIsAdmin, async (req, res) => {
+  const { name, email, password, role } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+
+    if (user) return res.status(400).json({ menssage: "User already existed" });
+    user = new User({ name, email, password, role: role || "customer" });
+    await user.save();
+
+    res.status(201).json({ message: "User created successfully", user });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
 export default router;
